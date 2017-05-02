fd5b73f155217a66380c93725175ef5e30aa047a
Ayodele Victor Muyiwa <ayodelevm@outlook.com>
2017-05-02 15:09:06 +0100

    converted functions to OOP class methods

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -2,145 +2,136 @@
 import path from 'path';
 import fs from 'fs';
 
+const allfiles = ['book-one.json', 'book-two.json', 'book-three.json'];
+const searchQuery = [["it's first string"], ['to', 'of'], 'reminscence'];
+
+
+class DataError {
+  constructor(message, data) {
+    this.message = message;
+    this.data = data;
+  }
+}
+
 class InvertedIndex {
   // Inverted index class attributes goes here
   constructor() {
-    this.filepath = '';
     this.fileContent = null;
+    this.currentPath = null;
+    this.createdIndex = null;
+    this.searchResult = null;
+    this.errors = [];
   }
 
-  getFilePath(filename) {
-    process.chdir('./');
-    const fullpath = path.join(process.cwd(), 'fixtures', filename);
-    this.filepath = fullpath;
-  }
-  readBookData() {
-    // filecontent read into a variable here
-    this.fileContent = JSON.parse(fs.readFileSync(this.filepath, 'utf8'));
-  }
-
-  checkStat() {
-    this.readBookData((err, fileData) => {
-      if (err) {
-        throw new Error(err);
-      } else {
-        console.log(fileData);
-        this.fileContent.push(JSON.parse(fileData));
+  *readBookData([...allFilenames]) {
+    for (this.filename of allFilenames) {
+      process.chdir('./');
+      this.fullpath = path.join(process.cwd(), 'fixtures', this.filename);
+      try {
+        this.fileContent = JSON.parse(fs.readFileSync(this.fullpath, 'utf8'));
+        yield {
+          filename: this.filename,
+          fileContent: this.fileContent
+        };
+      } catch (e) {
+        this.errors.push(new DataError('Invalid file', this.filename));
       }
-    });
+    }
   }
 
-  createIndex(filename) {
-    // logic goes in here
-    this.getFilePath(filename);
-    this.readBookData();
-    console.log(this.fileContent[0].title);
+  createIndex(fileObject = this.readBookData(allfiles)) {
+    const mappedIndex = {};
+    for (this.currentFile of fileObject) {
+      if (this.validateFileContent(this.currentFile.fileContent)) {
+        const wordsCollection = {};
+        for (const [index, newObject] of this.currentFile.fileContent.entries()) {
+          let titleTextArray = [];
+          titleTextArray = titleTextArray.concat(InvertedIndex.sanitizeData(newObject.title));
+          titleTextArray = titleTextArray.concat(InvertedIndex.sanitizeData(newObject.text));
+          titleTextArray.forEach((word) => {
+            if (wordsCollection.hasOwnProperty(word)) {
+              wordsCollection[word] = Array.from(new Set(wordsCollection[word].concat([index])));
+            } else {
+              wordsCollection[word] = [index];
+            }
+          });
+        }
+        mappedIndex[this.currentFile.filename] = JSON.parse(JSON.stringify(wordsCollection));
+      }
+      this.createdIndex = mappedIndex;
+    }
   }
 
-  searchIndex(index, fileName, searchTerms) {
-    // logic goes in here
+  *takeInSearchQuery([...allSearchQuery]) {
+    allSearchQuery = allSearchQuery.reduce((a, b) => a.concat(b), []);
+    for (const query of allSearchQuery) {
+      for (const newQuery of InvertedIndex.sanitizeData(query)) {
+        yield newQuery;
+      }
+    }
+  }
+
+  searchIndex(allQuery = this.takeInSearchQuery(searchQuery)) {
+    const queryResult = {};
+    for (const individualQuery of allQuery) {
+      const foundQuery = {};
+      Object.entries(this.createdIndex).forEach((objectArray) => {
+        const [filename, indexedData] = objectArray;
+        if (!indexedData.hasOwnProperty(individualQuery)) {
+          return;
+        }
+        if (indexedData.hasOwnProperty(individualQuery)) {
+          Object.assign(foundQuery, { [individualQuery]: indexedData[individualQuery] });
+        }
+        if (queryResult.hasOwnProperty(filename)) {
+          Object.assign(queryResult[filename], foundQuery);
+        } else {
+          queryResult[filename] = JSON.parse(JSON.stringify(foundQuery));
+        }
+      });
+    }
+    this.searchResult = queryResult;
+  }
+
+  validateFileContent(data) {
+    if (this.errors.length === 0) {
+      let hasErrors = false;
+      if (data.length > 0 && data.some(i =>
+        (JSON.stringify(i)[0]) !== '{' && JSON.stringify(i)[JSON.stringify(i).length - 1] !== '}')) {
+        this.errors.push(new DataError('file is not a JSON array', data));
+        hasErrors = true;
+      }
+
+      if (!hasErrors && data.some(i =>
+        JSON.stringify(i) === JSON.stringify({})) && Object.keys(data[0]).length === 0) {
+        this.errors.push(new DataError('JSON object cannot be empty or contain empty objects', data));
+        hasErrors = true;
+      }
+
+      if (!hasErrors && data.some(i =>
+        i.title === undefined || i.text === undefined)) {
+        this.errors.push(new DataError('Bad JSON Array format', data));
+        hasErrors = true;
+      }
+      return !hasErrors;
+    }
+  }
+
+  static sanitizeData(data) {
+    return data.toLowerCase().replace(/[^a-z A-Z 0-9]+/g, '').split(/[\s]\s*/);
   }
 
 }
 
 export default 'InvertedIndex';
 
-const docOne = new InvertedIndex();
+const indexOne = new InvertedIndex();
+indexOne.createIndex();
+console.log(indexOne.createdIndex);
+indexOne.searchIndex();
+console.log(indexOne.searchResult);
 
-/*
-=====================================================
-= My inverted index logic to be converted to classes
-=====================================================
-*/
-
-function* getFilePath([...filename]) {
-  for (let i of filename) {
-    process.chdir('./');
-    const fullpath = path.join(process.cwd(), 'fixtures', i);
-    yield {
-      filename: i,
-      fullpath: fullpath
-    };
-  }
-}
-
-function readBookData(filePath = getFilePath(filename)) {
-  let fileContent;
-  const map = {};
-
-  for (const object of filePath) {
-    try {
-      fileContent = JSON.parse(fs.readFileSync(object.fullpath, 'utf8'));
-    } catch (e) {
-      throw new Error('Invalid file');
-    }
-    if (fileContent.length >= 0 && fileContent.some(i =>
-      (JSON.stringify(i)[0]) !== '{' && JSON.stringify(i)[JSON.stringify(i).length - 1] !== '}')) {
-      throw new Error('file is not a JSON array');
-    }
-
-    if (fileContent.some(i =>
-      JSON.stringify(i) === JSON.stringify({})) && Object.keys(fileContent[0]).length === 0) {
-      throw new Error('JSON object cannot be empty or contain empty objects');
-    }
-
-    if (fileContent.some(i => i.title === undefined || i.text === undefined)) {
-      throw new Error('Bad JSON Array format');
-    }
-
-    const wordsCollection = {};
-    for (const [index, newObject] of fileContent.entries()) {
-      const titleArray = newObject.title.toLowerCase().replace(/[^a-z A-Z 0-9]+/g, '').split(/[\s]\s*/);
-      titleArray.forEach((word) => {
-        if (wordsCollection.hasOwnProperty(word)) {
-          wordsCollection[word] = Array.from(new Set(wordsCollection[word].concat([index])));
-        } else {
-          wordsCollection[word] = [index];
-        }
-      });
-      const textArray = newObject.text.toLowerCase().replace(/[^a-z A-Z 0-9]+/g, '').split(/[\s]\s*/);
-      textArray.forEach(word => {
-        if (wordsCollection.hasOwnProperty(word)) {
-          wordsCollection[word] = Array.from(new Set(wordsCollection[word].concat([index])));
-        } else {
-          wordsCollection[word] = [index];
-        }
-      });
-    }
-    map[object.filename] = JSON.parse(JSON.stringify(wordsCollection));
-  }
-  return map;
-}
-
-function* takeInSearchQuery([...searchQuery]) {
-  searchQuery = searchQuery.reduce((a, b) => a.concat(b), []);
-  for (const query of searchQuery) {
-    for (const newQuery of query.replace(/[^a-z A-Z 0-9]+/g, '').split(/[\s]\s*/)) {
-      yield newQuery.toLowerCase();
-    }
-  }
-}
-
-function searchIndex(allQuery = takeInSearchQuery(searchQuery)) {
-  const obj = readBookData();
-  const queryResult = {};
-  for (const individualQuery of allQuery) {
-    const foundQuery = {};
-    Object.entries(obj).forEach((objectArray) => {
-      const [filename, indexedData] = objectArray;
-      if (!indexedData.hasOwnProperty(individualQuery)) {
-        return;
-      }
-      if (indexedData.hasOwnProperty(individualQuery)) {
-        Object.assign(foundQuery, { [individualQuery]: indexedData[individualQuery] });
-      }
-      if (queryResult.hasOwnProperty(filename)) {
-        Object.assign(queryResult[filename], foundQuery);
-      } else {
-        queryResult[filename] = JSON.parse(JSON.stringify(foundQuery));
-      }
-    });
-  }
-  return queryResult;
+for(const error of indexOne.errors) {
+  console.log(error.message);
 }
 
