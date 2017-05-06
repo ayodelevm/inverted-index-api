7ed8f2a7d210d93b32cf2e7d76f98a023f6bbf3e
Ayodele Victor Muyiwa <ayodelevm@outlook.com>
2017-05-06 17:37:03 +0100

    Adding create and search routes, tested with postman

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-/* eslint linebreak-style: ["error", "windows"]*/
-
 import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
-import fs from 'fs';
 import multer from 'multer';
 
 import newIndex from './src/inverted-index';
@@ -29,50 +26,24 @@ server.use('/api', router);
 router.route('/create')
     .post(upload.array('allFiles'), (req, res) => {
       const allfiles = req.files;
-
-/*      const filenameAndPath = [];
-      allfiles.forEach(i => filenameAndPath.push({ [i.originalname]: i.path }));
-      filenameAndPath.forEach((eachFilenameAndPath) => {
-        for (const [filename, filepath] of Object.entries(eachFilenameAndPath)) {
-          const fileContent = JSON.parse(fs.readFileSync(filepath, 'utf8'));
-          // res.status(200).json(fileContent);
-        }
-      });
-/*      const filenameAndPath = [];
-      allfiles.forEach(i => filenameAndPath.push({ [i.originalname]: i.path }));
-      for (const eachFilenameAndPath of filenameAndPath) {
-        console.log(Object.entries(eachFilenameAndPath));
-        for (const [filename, filepath] of Object.entries(eachFilenameAndPath)) {
-          const fileContent = JSON.parse(fs.readFileSync(filepath, 'utf8'));
-          res.status(200).json(filename);
-        }
-      }*/
-
       const fileNameAndContent = newInvertedIndex.readBookDataApiMulter(allfiles);
       newInvertedIndex.createIndex(fileNameAndContent);
-      res.status(200).json(newInvertedIndex.createdIndex);
+      res.status(200).json({
+        processedFiles: newInvertedIndex.createdIndex,
+        fileErrors: newInvertedIndex.errors
+      });
     });
-/*
-let newArr = [];
-Array.from(arr).forEach(a => newArr.push({[a.originalname]: a.path}));
-
-router.post('/create', (req, res) => {
-
-  upload(req, res, (err) => {
-    if (err) {
-      console.error(err);
-      return
-    }
-
-    res.status(200).send('hi you');
-  });
-});*/
 
 router.route('/search')
     .post((req, res) => {
-      const searchQueries = req.body;
-      const individualQueries = newInvertedIndex.takeInSearchQuery(searchQueries);
-      newInvertedIndex.searchIndex(individualQueries);
+      const searchQueries = req.body.searchTerms;
+      const filename = req.body.fileName;
+      const index = newInvertedIndex.createdIndex;
+      if (req.body.fileName === undefined) {
+        newInvertedIndex.searchIndex(index, searchQueries);
+      } else {
+        newInvertedIndex.searchIndex(index, filename, searchQueries);
+      }
       res.status(200).json(newInvertedIndex.searchResult);
     });
 
