a2db1fe2d9686e709e946642b7deac0204338128
Ayodele Victor Muyiwa <ayodelevm@outlook.com>
2017-05-11 17:04:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -56,8 +56,8 @@ router.route('/search')
     });
 
 
-server.listen();
-console.log('server running at port: ', );
+server.listen(process.env.PORT_PROD);
+console.log('server running at port: ', process.env.PORT_PROD);
 
 export default server;
 
